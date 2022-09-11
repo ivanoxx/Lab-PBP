da40ec0f528ac56360cbc46a6cf12f0eb937eb75
dafinr <dafinr13@gmail.com>
2022-09-11 10:01:42 +0700

    Lab 1 done

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_item': data_barang_katalog,
+        'nama': 'Dafi Nafidz Radhiyya',
+        'student_id': '2106701564'
+    }
+    return render(request, "katalog.html", context)
