1e7f6a0b10b6276f90732ece1c085a64872b6953
ivanoxx <dafinr13@gmail.com>
2022-09-21 11:27:05 +0700

    Fix html routing

--- mywatchList/urls.py
+++ mywatchList/urls.py
@@ -4,7 +4,7 @@ from mywatchList.views import show_mywatchList, show_xml, show_json, show_json_b
 app_name = 'mywatchList'
 
 urlpatterns = [
-    path('', show_mywatchList, name='show_mywatchList'),
+    path('html/', show_mywatchList, name='show_mywatchList'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
