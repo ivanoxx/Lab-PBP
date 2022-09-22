11f225e0b60bf43bfbc30ae5b3522fc43cde9d42
ivanoxx <dafinr13@gmail.com>
2022-09-22 08:07:02 +0700

    Add unit testing

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
 # Create your tests here.
+class TestMyWatchList(TestCase):
+    def test_html_url_is_exist(self):
+        response = Client().get(reverse('mywatchlist:show_mywatchlist'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_xml_url_is_exist(self):
+        response = Client().get(reverse('mywatchlist:show_xml'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_json_url_is_exist(self):
+        response = Client().get(reverse('mywatchlist:show_json'))
+        self.assertEqual(response.status_code, 200)
