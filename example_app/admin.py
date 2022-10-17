8496888350236afb97a309751a066fe5ab3878af
ivanoxx <dafinr13@gmail.com>
2022-10-17 19:34:07 +0700

    Register models to Django Admin

--- example_app/admin.py
+++ example_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from katalog.models import CatalogItem
+from mywatchlist.models import WatchAttr
+from todolist.models import Task
 
 # Register your models here.
+
+admin.site.register(CatalogItem)
+admin.site.register(WatchAttr)
+admin.site.register(Task)
